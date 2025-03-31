6c72fafc6098d109b83e096b32f6efeb5b63450f
CELSO MARCOS FURTADO <sn1006137@CFP127ALUNO.LOCAL>
2025-03-31 11:23:25 -0300

    Iniciando a construção da tela com swing

---
+++ src/jandira/br/sp/tabuada/Main.java
@@ -0,0 +1,14 @@
+package jandira.br.sp.tabuada;
+
+import jandira.br.sp.tabuada.gui.TelaTabuada;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		TelaTabuada tela = new TelaTabuada();
+		tela.criarTela();
+
+	}
+
+}
